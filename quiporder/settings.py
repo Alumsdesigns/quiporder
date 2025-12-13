62764a03f67c2d0b425e4cbbc1d68941a884d96d
Alumsdesigns <alum.damaris@gmail.com>
2025-12-13 22:03:11 +0000

    feat: add CustomUser model and fix settings configuration

--- quiporder/settings.py
+++ quiporder/settings.py
@@ -98,6 +98,11 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
+# Default primary key field type
+# https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -114,8 +119,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'quiporder.wsgi.application'
-
-
 
 
 # Database
@@ -164,12 +167,3 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.2/howto/static-files/
-
-STATIC_URL = 'static/'
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
--- users/models.py
+++ users/models.py
@@ -1,7 +1,40 @@
 # Create your models here.
 # Database tables (User, Therapist, Patient)
 
-from django.db import models
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 
+class CustomUser(AbstractUser):
+    """
+    Custom user model that extends Django's AbstractUser.
+    Adds user_type field to distinguish between Therapists and Patients.
+    """
+    
+    USER_TYPE_CHOICES = [
+        ('THERAPIST', 'Occupational Therapist'),
+        ('PATIENT', 'Patient'),
+    ]
+    
+    user_type = models.CharField(
+        max_length=10,
+        choices=USER_TYPE_CHOICES,
+        help_text="User role: Therapist or Patient"
+    )
+    
+    # Use email for login instead of username
+    email = models.EmailField(unique=True)
+    
+    # USERNAME_FIELD tells Django to use email for authentication
+    USERNAME_FIELD = 'email'
+    
+    # REQUIRED_FIELDS are prompted for when creating superuser
+    # email is already the USERNAME_FIELD, so don't include it here
+    REQUIRED_FIELDS = ['username', 'user_type']
+    
+    class Meta:
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
+    
+    def __str__(self):
+        return f"{self.email} ({self.get_user_type_display()})"
